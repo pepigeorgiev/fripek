a03b4161451275007bd5e2902ccfd9e344454d69
Simply Done <pepi.georgiev@yahoo.com>
2025-01-30 17:43:02 +0100

    izmeni vo pwa

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -184,11 +184,11 @@
     </script>
 
     <link rel="manifest" href="/manifest.json">
-    <meta name="theme-color" content="#ffffff">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="default">
-    <meta name="apple-mobile-web-app-title" content="Фри-пек">
     <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="icon" type="image/png" sizes="192x192" href="/images/icon-192x192.png">
+    <link rel="apple-touch-icon" href="/images/icon-192x192.png">
 
     <!-- iOS specific tags -->
     <link rel="apple-touch-icon" href="/images/icon-192x192.png">
