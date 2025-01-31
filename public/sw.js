b8c8182f6df7db047148f328369a160364e8da53
Simply Done <pepi.georgiev@yahoo.com>
2025-01-31 16:35:45 +0100

    pwa

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,9 @@
-const CACHE_NAME = 'fripek-v1';
+const CACHE_NAME = 'fripek-v2';
 const DOMAIN = 'https://fripekapp.mk';
 
 const urlsToCache = [
     '/',
+    '/install-app',
     '/css/app.css',
     '/js/app.js',
     '/images/icon-192x192.png',
@@ -11,65 +12,45 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
-    console.log('Service Worker installing.');
+    console.log('Service Worker: Installing...');
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache.map(url => DOMAIN + url));
+                console.log('Service Worker: Caching Files...');
+                return cache.addAll(urlsToCache);
             })
+            .then(() => self.skipWaiting())
     );
-    self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
-    console.log('Service Worker activating.');
+    console.log('Service Worker: Activated');
     event.waitUntil(
-        caches.keys().then(keys => Promise.all(
-            keys.map(key => {
-                if (key !== CACHE_NAME) {
-                    return caches.delete(key);
-                }
-            })
-        ))
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        console.log('Service Worker: Clearing Old Cache');
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
     );
 });
 
 self.addEventListener('fetch', event => {
+    console.log('Service Worker: Fetching');
     event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                // Return cached response if found
-                if (response) {
-                    return response;
-                }
-
-                // Clone the request because it can only be used once
-                const fetchRequest = event.request.clone();
-
-                return fetch(fetchRequest)
-                    .then(response => {
-                        // Check if valid response
-                        if (!response || response.status !== 200 || response.type !== 'basic') {
-                            return response;
-                        }
-
-                        // Clone the response because it can only be used once
-                        const responseToCache = response.clone();
-
-                        caches.open(CACHE_NAME)
-                            .then(cache => {
-                                cache.put(event.request, responseToCache);
-                            });
-
-                        return response;
-                    })
-                    .catch(() => {
-                        // Return offline page if no internet
-                        if (event.request.mode === 'navigate') {
-                            return caches.match('/offline.html');
-                        }
+        fetch(event.request)
+            .then(res => {
+                const resClone = res.clone();
+                caches.open(CACHE_NAME)
+                    .then(cache => {
+                        cache.put(event.request, resClone);
                     });
+                return res;
             })
+            .catch(() => caches.match(event.request))
     );
 }); 
